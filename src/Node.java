d3feae20cd05a4cfdf6347372aa1a134f75664d7
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-12-19 21:01:29 +0100

    Add inner classes so that announcing doesn't lock the calling node

--- src/Node.java
+++ src/Node.java
@@ -18,9 +18,65 @@ class Node {
     private int aptitude;
     // This Node's RMIClient
     private final RMIClient rmiClient;
+
+    public boolean isAnnouncing() {
+        return announcing;
+    }
+
     // Contains whether or not this Node is announcing
     private boolean announcing;
 
+    /**
+     * Inner class used to make announces to the RMIClient, so that the calling Node can be released
+     */
+    private class AnnouncerThread implements Runnable {
+        RMIClient rmiClient;
+        int id, aptitude;
+        AnnouncerThread(RMIClient rmiClient, int id, int aptitude) {
+            this.rmiClient = rmiClient;
+            this.id = id;
+            this.aptitude = aptitude;
+        }
+
+        @Override
+        public void run() {
+            try {
+                this.rmiClient.announce(this.id, this.aptitude);
+            } catch (RemoteException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Inner class used to send results to the RMIClient, so that the calling Node can be released
+     */
+    private class ResultThread implements Runnable {
+
+        private RMIClient rmiClient;
+        private int id;
+        ResultThread(RMIClient rmiClient, int id) {
+            this.rmiClient = rmiClient;
+            this.id = id;
+        }
+        @Override
+        public void run() {
+            try {
+                this.rmiClient.result(id);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Constructor. Needs the number of election processes the client should start
+     *
+     * @param numberOfElectionProcesses
+     * @throws RemoteException
+     * @throws MalformedURLException
+     * @throws NotBoundException
+     */
     Node(int numberOfElectionProcesses) throws RemoteException, MalformedURLException, NotBoundException {
         this.id = numberOfNodes++;
         this.announcing = false;
@@ -59,19 +115,21 @@ class Node {
                 Logger.getLogger(this.getClass().getSimpleName()).log(Level.INFO, "Node " + this.id + " is elected");
                 this.announcing = true;
                 this.aptitude = 0;
-                this.rmiClient.announce(this.id, this.aptitude);
+                new Thread(new AnnouncerThread(this.rmiClient, this.id, this.aptitude)).start();
             }
 
         // Check if this node has been elected by all other nodes, full circle
         } else if (this.id == id) {
             Logger.getLogger(this.getClass().getSimpleName()).log(Level.INFO, "Elect in Node " + this.id + " full circle, sending result");
+
+
             rmiClient.result(this.id);
         // Caller node has a higher aptitude
         } else {
             Logger.getLogger(this.getClass().getSimpleName()).log(Level.INFO, id + " is elected");
             this.announcing = true;
             this.aptitude++;
-            this.rmiClient.announce(id, apt);
+            new Thread(new ResultThread(this.rmiClient, this.id)).start();
         }
     }
 
@@ -85,7 +143,7 @@ class Node {
         Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Node " + this.id + " has received result: Node " + resultNodeId + " was elected");
         this.announcing = false;
         if (this.id != resultNodeId) {
-            rmiClient.result(resultNodeId);
+            new Thread(new ResultThread(this.rmiClient, resultNodeId)).start();
         } else {
             Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Received result, I (Node " + this.id + "), have been elected");
         }
